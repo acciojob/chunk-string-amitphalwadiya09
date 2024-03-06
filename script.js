3431a4e8184ec146d5cac03f0e0919e6dbf1e046
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-03-06 18:41:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function stringChop(str, size) {
   // your code here
+	for(let i=0;i<str.length;i++)
+		{
+			let result=[]
+			for(let j=0;j<size && i<str.length;j++)
+				{
+					let char=char+str[i];
+					i++;
+				}
+			result.push(char);
+		}
+	
 }
 
 // Do not change the code below
